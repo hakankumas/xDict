ab3b6fae6ad113bc717526de6884316b9fa4d7c6
Hakan KUMAŞ <hakankumas54@gmail.com>
2024-09-26 17:34:24 +0300

    Header edited.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,33 +4,50 @@ function Header({ sessionData }) {
     // console.log("Header is rendered.");
     return (
         <div className="header">
-            <div className="section1">
-                <Link className="link home" to={sessionData ? "/user/" : "/"}>
-                    Home
-                </Link>
-                <Link className="link" to="/about">
-                    About
-                </Link>
-                <Link className="link" to="/services">
-                    Services
-                </Link>
-            </div>
-            <div className="section2">
-                {sessionData ? (
-                    <Link className="link logout" to="/logout">
-                        Logout
-                    </Link>
-                ) : (
-                    <>
-                        <Link className="link register" to="/register">
-                            Register
+            {sessionData ? (
+                <>
+                    <div className="section1">
+                        <Link className="link home" to="/user/">
+                            xDict
                         </Link>
-                        <Link className="link login" to="/login">
-                            Login
+                        <Link className="link" to="/user/">
+                            Home
                         </Link>
-                    </>
-                )}
-            </div>
+                    </div>
+                    <div className="section2">
+                        <Link className="link logout" to="/logout">
+                            Logout
+                        </Link>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="section1">
+                        <Link className="link home" to="/">
+                            xDict
+                        </Link>
+                        <Link className="link" to="/">
+                            Home
+                        </Link>
+                        <Link className="link" to="/about">
+                            About
+                        </Link>
+                        <Link className="link" to="/services">
+                            Services
+                        </Link>
+                    </div>
+                    <div className="section2">
+                        <>
+                            <Link className="link register" to="/register">
+                                Register
+                            </Link>
+                            <Link className="link login" to="/login">
+                                Login
+                            </Link>
+                        </>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
