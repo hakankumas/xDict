324ebf8ac7dc12a7996c6b81bc047da72a27d343
Hakan KUMAŞ <hakankumas54@gmail.com>
2024-10-14 01:41:09 +0300

    Number of posts and topics are shown on the profile.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid2";
 import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import MentionItem from "./MentionItem";
 import { postOfUser } from "../redux/features/post/postSlice";
@@ -19,32 +18,90 @@ function Profile() {
     const { posts, postsOfUser } = useSelector((state) => state.post);
     const dispatch = useDispatch();
     const username = localStorage?.getItem("username");
-
+    const [postSharedByUser, setPostSharedByUser] = useState("");
+    const [topicOfPostSharedByUser, setTopicOfPostSharedByUser] = useState("");
     useEffect(() => {
         dispatch(postOfUser(username));
     }, [posts]);
+
+    useEffect(() => {
+        if (postsOfUser) {
+            const postSharedByUser_x = postsOfUser.length;
+            const topicOfPostSharedByUser_x = new Set(
+                postsOfUser.map((post) => post.topic._id)
+            ).size;
+
+            setPostSharedByUser(postSharedByUser_x);
+            setTopicOfPostSharedByUser(topicOfPostSharedByUser_x);
+        }
+    }, [postsOfUser]);
     return (
         <div className="profile">
             <Grid container>
-                <Grid size={12}>
-                    <Stack spacing={1} sx={{ marginTop: "1%" }}>
-                        <ProfileInfo />
-                    </Stack>
-
-                    <Stack spacing={1} sx={{ marginTop: "10%" }}>
+                <Grid size={12} spacing={1} sx={{ marginTop: "1%" }}>
+                    <ProfileInfo />
+                </Grid>
+                {!postSharedByUser ? (
+                    <Grid size={12} spacing={1} sx={{ marginTop: "10%" }}>
                         <Item>
-                            <Typography variant="h6" component="div">
-                                My Last Mentions
+                            <Typography
+                                variant="h6"
+                                sx={{
+                                    textAlign: "center",
+                                    fontWeight: "bold",
+                                    fontStyle: "italic",
+                                }}
+                            >
+                                No Mention
                             </Typography>
                         </Item>
-                        <Item>
-                            {postsOfUser &&
-                                postsOfUser.map((item) => (
-                                    <MentionItem key={item._id} item={item} />
-                                ))}
-                        </Item>
-                    </Stack>
-                </Grid>
+                    </Grid>
+                ) : (
+                    <>
+                        <Grid className="my-count">
+                            <Grid>
+                                <Item>
+                                    <Typography variant="caption">
+                                        TOPIC
+                                    </Typography>
+                                </Item>
+                                <Item>
+                                    <Typography variant="h6">
+                                        {topicOfPostSharedByUser}
+                                    </Typography>
+                                </Item>
+                            </Grid>
+                            <Grid>
+                                <Item>
+                                    <Typography variant="caption">
+                                        POST
+                                    </Typography>
+                                </Item>
+                                <Item>
+                                    <Typography variant="h6">
+                                        {postSharedByUser}
+                                    </Typography>
+                                </Item>
+                            </Grid>
+                        </Grid>
+                        <Grid size={12} spacing={1} sx={{ marginTop: "7%" }}>
+                            <Item sx={{ marginBottom: "10px" }}>
+                                <Typography variant="h6">
+                                    My Last Mentions
+                                </Typography>
+                            </Item>
+                            <Item>
+                                {postsOfUser &&
+                                    postsOfUser.map((item) => (
+                                        <MentionItem
+                                            key={item._id}
+                                            item={item}
+                                        />
+                                    ))}
+                            </Item>
+                        </Grid>
+                    </>
+                )}
             </Grid>
         </div>
     );
