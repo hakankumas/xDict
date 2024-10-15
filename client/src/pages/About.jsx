32f8b42d3dba736f5eab846a6b9dff18b3d5cbf5
Hakan KUMAŞ <hakankumas54@gmail.com>
2024-10-16 02:06:13 +0300

    Services and About Pages edited.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,23 +6,126 @@ function About() {
         <div>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={9}>
+                    <Grid size={12}>
                         <div>
-                            <h1>About Page - Content</h1>
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    padding: "20px 0px",
+                                }}
+                            >
+                                <h1>About</h1>
+                            </div>
                             <hr />
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Molestias aliquid dolorum
-                                saepe, numquam mollitia fuga ducimus cumque,
-                                sapiente eius ex facere cum tenetur
-                                reprehenderit quas quo ab placeat. Itaque
-                                aliquam obcaecati corporis molestiae sunt nam,
-                                excepturi doloribus fuga tempore quis id magnam
-                                et quidem vitae ducimus commodi alias aut ex!
-                            </p>
+                            <div
+                                style={{
+                                    textAlign: "justify",
+                                    padding: "20px 30px",
+                                    textIndent: "50px",
+                                }}
+                            >
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Molestias aliquid dolorum
+                                    saepe, numquam mollitia fuga ducimus cumque,
+                                    sapiente eius ex facere cum tenetur
+                                    reprehenderit quas quo ab placeat. Itaque
+                                    aliquam obcaecati corporis molestiae sunt
+                                    nam, excepturi doloribus fuga tempore quis
+                                    id magnam et quidem vitae ducimus commodi
+                                    alias aut ex!
+                                </p>
+                                <p>
+                                    Natus id consectetur ipsam consequuntur,
+                                    numquam magnam dolores quae nobis nulla eum
+                                    hic, repellat similique fugiat labore
+                                    aliquam possimus at optio quam, eligendi
+                                    recusandae molestiae sint aut. Quas,
+                                    voluptas explicabo. Modi dolorum molestias
+                                    recusandae minima expedita ipsam odio! Atque
+                                    eius, doloribus error ipsam ad officiis hic!
+                                    Exercitationem, sit fugiat iste cupiditate
+                                    nobis nihil molestias ipsa repellat omnis
+                                    accusantium laboriosam ipsam odio vitae quo
+                                    deleniti nam sequi id deserunt qui quam
+                                    beatae sapiente. Alias molestias facilis
+                                    placeat ad veritatis eum non reiciendis
+                                    cumque nesciunt possimus ipsa consequuntur
+                                    architecto corporis totam dolorem, esse
+                                    fugiat.
+                                </p>
+                                <p>
+                                    Autem molestiae corporis, beatae quis
+                                    perspiciatis eveniet vel minus dolorem
+                                    libero eaque! Quo culpa labore fugit
+                                    laudantium! Dolore voluptas consequatur
+                                    quam, labore delectus animi numquam,
+                                    possimus molestiae dolor, incidunt ipsam
+                                    ducimus? Cupiditate, repudiandae fugiat
+                                    perferendis magnam nesciunt ratione quis,
+                                    illo quia modi autem ipsam, velit
+                                    praesentium. Totam veritatis possimus
+                                    laudantium excepturi blanditiis quisquam
+                                    doloribus pariatur corrupti non. Veniam,
+                                    quasi? Unde nesciunt voluptatibus nihil
+                                    voluptates ab aut recusandae qui itaque
+                                    laudantium harum labore odio esse nobis
+                                    exercitationem veniam officiis tenetur
+                                    aperiam, error odit voluptas. Exercitationem
+                                    doloribus iusto cumque pariatur similique
+                                    fugiat vero dolorem sunt qui neque. Officia
+                                    cumque ipsam cum alias excepturi reiciendis
+                                    incidunt, numquam aut in quo deserunt? Amet
+                                    soluta veniam esse non quam excepturi
+                                    corporis, sapiente ad reprehenderit fugit
+                                    cumque autem itaque magni dolorum nisi porro
+                                    odio deleniti architecto mollitia voluptas
+                                    optio? Similique totam fugit animi
+                                    praesentium quos inventore quibusdam!
+                                    Ducimus, ipsa eius saepe quaerat minima,
+                                    incidunt commodi molestiae ipsam mollitia in
+                                    suscipit. Officia magni inventore in ducimus
+                                    obcaecati perferendis debitis ea nemo
+                                    nostrum ratione, enim, voluptate provident
+                                    fugiat consequatur eos quaerat aliquid
+                                    corrupti expedita aspernatur quisquam modi
+                                    fugit pariatur mollitia! Praesentium,
+                                    voluptates sint deleniti consequatur odio
+                                    nisi alias facilis porro.
+                                </p>
+                                <p>
+                                    Excepturi repellat officia fugit unde
+                                    nostrum cupiditate tempore cumque? Id
+                                    voluptatibus eligendi at, odit accusamus
+                                    eius veniam iure eaque laborum, maiores
+                                    numquam harum mollitia perspiciatis fuga
+                                    nostrum vitae neque ipsum rem praesentium
+                                    nesciunt hic, quam accusantium expedita
+                                    libero. Sint vel soluta exercitationem.
+                                    Consequuntur itaque beatae corporis est
+                                    dolore eius incidunt, iure quaerat
+                                    voluptate, sunt assumenda? Nemo repudiandae
+                                    provident soluta vero iure, aut doloremque
+                                    saepe eligendi est praesentium. Consequatur
+                                    ab commodi corrupti aliquam dicta nihil vero
+                                    itaque voluptatum quia ipsam qui cupiditate
+                                    quo quos rem reiciendis, tenetur
+                                    necessitatibus iure nesciunt! Minus, est
+                                    perferendis.
+                                </p>
+                                <p>
+                                    Eum numquam harum explicabo deserunt omnis
+                                    maxime, quod similique, dolorum impedit
+                                    recusandae repudiandae. Magnam voluptatum
+                                    minus hic nostrum? Molestiae consectetur
+                                    deleniti pariatur? Quibusdam sint, possimus
+                                    magni tenetur molestias fugit hic? Possimus
+                                    accusantium illo id nemo temporibus
+                                    provident adipisci, minus corporis.
+                                </p>
+                            </div>
                         </div>
                     </Grid>
-                    <Grid size={3}>Sidebar</Grid>
                 </Grid>
             </Container>
         </div>
--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -7,23 +7,130 @@ function Services() {
         <div>
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
-                    <Grid size={9}>
+                    <Grid size={12}>
                         <div>
-                            <h1>Services Page - Content</h1>
+                            <div
+                                style={{
+                                    textAlign: "center",
+                                    padding: "20px 0px",
+                                }}
+                            >
+                                <h1>Services</h1>
+                            </div>
                             <hr />
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Molestias aliquid dolorum
-                                saepe, numquam mollitia fuga ducimus cumque,
-                                sapiente eius ex facere cum tenetur
-                                reprehenderit quas quo ab placeat. Itaque
-                                aliquam obcaecati corporis molestiae sunt nam,
-                                excepturi doloribus fuga tempore quis id magnam
-                                et quidem vitae ducimus commodi alias aut ex!
-                            </p>
+                            <div
+                                style={{
+                                    textAlign: "justify",
+                                    padding: "20px 30px",
+                                    textIndent: "50px",
+                                }}
+                            >
+                                <p>
+                                    Itaque aliquam obcaecati corporis molestiae
+                                    sunt nam, excepturi doloribus fuga tempore
+                                    quis id magnam et quidem vitae ducimus
+                                    commodi alias aut ex! Lorem ipsum dolor sit
+                                    amet consectetur adipisicing elit. Assumenda
+                                    ex eius cumque necessitatibus blanditiis
+                                    iure cupiditate repellat mollitia, quaerat
+                                    placeat totam? Numquam incidunt blanditiis
+                                    quibusdam a nostrum vero adipisci minima
+                                    saepe, amet cum animi earum ullam atque odit
+                                    modi sit dicta, omnis deserunt quisquam?
+                                    Beatae aspernatur omnis repudiandae quas
+                                    possimus!
+                                </p>
+                                <p>
+                                    Modi dolorum molestias recusandae minima
+                                    expedita ipsam odio! Atque eius, doloribus
+                                    error ipsam ad officiis hic! Exercitationem,
+                                    sit fugiat iste cupiditate nobis nihil
+                                    molestias ipsa repellat omnis accusantium
+                                    laboriosam ipsam odio vitae quo deleniti nam
+                                    sequi id deserunt qui quam beatae sapiente.
+                                    Alias molestias facilis placeat ad veritatis
+                                    eum non reiciendis cumque nesciunt possimus
+                                    ipsa consequuntur architecto corporis totam
+                                    dolorem, esse fugiat.
+                                </p>
+                                <p>
+                                    Totam veritatis possimus laudantium
+                                    excepturi blanditiis quisquam doloribus
+                                    pariatur corrupti non. Veniam, quasi? Unde
+                                    nesciunt voluptatibus nihil voluptates ab
+                                    aut recusandae qui itaque laudantium harum
+                                    labore odio esse nobis exercitationem veniam
+                                    officiis tenetur aperiam, error odit
+                                    voluptas. Exercitationem doloribus iusto
+                                    cumque pariatur similique fugiat vero
+                                    dolorem sunt qui neque. Officia cumque ipsam
+                                    cum alias excepturi reiciendis incidunt,
+                                    numquam aut in quo deserunt? Amet soluta
+                                    veniam esse non quam excepturi corporis,
+                                    sapiente ad reprehenderit fugit cumque autem
+                                    itaque magni dolorum nisi porro odio
+                                    deleniti architecto mollitia voluptas optio?
+                                    Similique totam fugit animi praesentium quos
+                                    inventore quibusdam! Ducimus, ipsa eius
+                                    saepe quaerat minima, incidunt commodi
+                                    molestiae ipsam mollitia in suscipit.
+                                    Officia magni inventore in ducimus obcaecati
+                                    perferendis debitis ea nemo nostrum ratione,
+                                    enim, voluptate provident fugiat consequatur
+                                    eos quaerat aliquid corrupti expedita
+                                    aspernatur quisquam modi fugit pariatur
+                                    mollitia! Praesentium, voluptates sint
+                                    deleniti consequatur odio nisi alias facilis
+                                    porro.
+                                </p>
+                                <p>
+                                    Nemo repudiandae provident soluta vero iure,
+                                    aut doloremque saepe eligendi est
+                                    praesentium. Consequatur ab commodi corrupti
+                                    aliquam dicta nihil vero itaque voluptatum
+                                    quia ipsam qui cupiditate quo quos rem
+                                    reiciendis, tenetur necessitatibus iure
+                                    nesciunt! Minus, est perferendis. Lorem
+                                    ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Non ab architecto eaque.
+                                    Minus porro rem assumenda qui repudiandae
+                                    natus doloremque, possimus amet quo cum.
+                                    Eaque aperiam, eveniet eos illum omnis
+                                    pariatur corrupti quo, saepe ad dolores
+                                    tempore, ipsum aspernatur vero?
+                                </p>
+                                <p>
+                                    Molestiae consectetur deleniti pariatur?
+                                    Quibusdam sint, possimus magni tenetur
+                                    molestias fugit hic? Possimus accusantium
+                                    illo id nemo temporibus provident adipisci,
+                                    minus corporis. Necessitatibus officia omnis
+                                    sequi magni magnam quo, temporibus alias,
+                                    consectetur repudiandae dolore, quis dolores
+                                    laudantium non architecto aliquid similique
+                                    eos fugiat dolorem rerum pariatur nulla
+                                    enim. In, ullam dolorum. Fuga, modi!
+                                    Aliquid, placeat aliquam doloribus nulla
+                                    repellendus autem sapiente ad veniam nostrum
+                                    sint deserunt dolores minus cum? Sunt
+                                    tempore harum dolores, doloribus obcaecati
+                                    quisquam at quod soluta eius error maxime
+                                    ipsa laborum quis eum praesentium
+                                    perspiciatis necessitatibus distinctio autem
+                                    repellendus? Sequi quod qui ad aperiam optio
+                                    odit reiciendis iste rerum illo officia rem
+                                    tenetur quaerat neque consequuntur dolorum,
+                                    quas consequatur beatae velit alias. Sit
+                                    quod expedita ex praesentium perspiciatis
+                                    repellendus voluptatibus, vero quidem
+                                    aperiam, ullam culpa, possimus consequatur
+                                    enim sapiente molestias dicta saepe magni
+                                    beatae blanditiis quasi voluptas quis
+                                    cupiditate.
+                                </p>
+                            </div>
                         </div>
                     </Grid>
-                    <Grid size={3}>Sidebar</Grid>
                 </Grid>
             </Container>
         </div>
